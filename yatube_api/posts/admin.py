1e7ae835a00b9a36befec07dc0881f02a9a6c384
sonyaplff <sofarasskazova1@gmail.com>
2026-04-28 17:17:07 +0500

    Fix trailing whitespace and newlines - final version

--- yatube_api/posts/admin.py
+++
@@ -1,8 +0,0 @@
-"""Admin configuration for posts app."""
-from django.contrib import admin
-from .models import Post, Group, Comment, Follow
-
-admin.site.register(Post)
-admin.site.register(Group)
-admin.site.register(Comment)
-admin.site.register(Follow)
