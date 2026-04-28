353e513cb70631859b451f93e2076b14e7d07b80
sonyaplff <sofarasskazova1@gmail.com>
2026-04-28 18:01:44 +0500

    Recreate models.py and permissions.py via command line

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,15 +1,7 @@
-"""Custom permissions for API."""
-from rest_framework import permissions
-
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.method in permissions.SAFE_METHODS or obj.author == request.user
+from rest_framework import permissions 
+ 
+class IsAuthorOrReadOnly(permissions.BasePermission): 
+    def has_object_permission(self, request, view, obj): 
+        if request.method in permissions.SAFE_METHODS: 
+            return True 
+        return obj.author == request.user 
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,73 +1,20 @@
-"""Models for posts app."""
-from django.contrib.auth import get_user_model
-from django.db import models
-
-User = get_user_model()
-
-
-class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
-
-    class Meta:
-        ordering = ['id']
-
-    def __str__(self):
-        return self.title
-
-
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts'
-    )
-    group = models.ForeignKey(
-        Group, on_delete=models.SET_NULL, blank=True, null=True,
-        related_name='posts'
-    )
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True
-    )
-
-    class Meta:
-        ordering = ['-pub_date']
-
-    def __str__(self):
-        return self.text
-
-
-class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments'
-    )
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments'
-    )
-    text = models.TextField()
-    created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True
-    )
-
-    class Meta:
-        ordering = ['-created']
-
-
-class Follow(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='follower'
-    )
-    following = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='following'
-    )
-
-    class Meta:
-        ordering = ['-id']
-        constraints = [
-            models.UniqueConstraint(
-                fields=['user', 'following'], name='unique_follow'
-            )
-class Follow(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    following = models.ForeignKey(User, on_delete=models.CASCADE)
+from django.db import models 
+from django.contrib.auth import get_user_model 
+ 
+User = get_user_model() 
+ 
+class Group(models.Model): 
+    title = models.CharField(max_length=200) 
+ 
+class Post(models.Model): 
+    text = models.TextField() 
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+ 
+class Comment(models.Model): 
+    text = models.TextField() 
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE) 
+ 
+class Follow(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    following = models.ForeignKey(User, on_delete=models.CASCADE) 
