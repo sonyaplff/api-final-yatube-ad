d8010b6694d645111dd6df71d5e0bb7b81845a32
sonyaplff <sofarasskazova1@gmail.com>
2026-04-28 17:26:35 +0500

    Complete rewrite of models.py and permissions.py without any whitespace issues

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -68,5 +68,4 @@ class Follow(models.Model):
             models.UniqueConstraint(
                 fields=['user', 'following'], name='unique_follow'
             )
-        ] 
-        
+        ]
